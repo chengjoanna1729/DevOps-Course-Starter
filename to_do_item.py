a76a6420e14e8f1135044a78ba7937edae69a7a4
Joanna Cheng <joanna.cheng@softwire.com>
2020-07-21 17:52:40 +0100

    Add done items splitting logic

--- to_do_item.py
+++ to_do_item.py
@@ -1,17 +1,23 @@
 from enum import Enum
+from datetime import datetime
+
+def parse_trello_date(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+
 class Status(Enum):
     TO_DO = "To Do"
     DOING = "Doing"
     DONE = "Done"
 
 class ToDoItem:
-    def __init__(self, id, title, status, description):
+    def __init__(self, id, title, status, description, last_modified):
         self.id = id
         self.title = title
         self.status = status
         self.description = description
+        self.last_modified = last_modified
 
     @classmethod
     def fromTrelloCard(cls, card, status):
-        return cls(card['id'], card['name'], status, card['desc'])
+        return cls(card['id'], card['name'], status, card['desc'], parse_trello_date(card['dateLastActivity']))
         
--- view_model.py
+++ view_model.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from to_do_item import Status 
 
 class ViewModel:
@@ -20,3 +21,14 @@ class ViewModel:
     def done_items(self):
         return [item for item in self._items if item.status == Status.DONE]
         
+    @property
+    def show_all_done_items(self):
+        return len(self.done_items) < 5
+
+    @property
+    def recent_done_items(self):
+        return [item for item in self.done_items if datetime.now() - timedelta(days=1) < item.last_modified]
+
+    @property
+    def older_done_items(self):
+        return list(set(self.done_items) - set(self.recent_done_items))
